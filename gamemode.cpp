63a230a1b1c79711671bb86883c825ae13244c93
Maxim <progon3@mail.ru>
2015-02-19 20:25:05 +0200

    Create gamemode.cpp

---
+++ gamemode.cpp
@@ -0,0 +1,33 @@
+#include "gamemode.h"
+#include <iostream>
+
+GameMode::GameMode(GameState* parent)
+{
+    nextState = parent;
+    parent->addChild(this);
+}
+
+
+void GameMode::draw()
+{
+    std::cout << "Game: here performed physical computations and graphics refresh." << std::endl;
+}
+
+
+void GameMode::update(float)
+{
+    static int count = 0;
+
+    if(count > 3)
+        activeState = false;
+    else
+    {
+        draw();
+        ++count;
+    }
+}
+
+
+void GameMode::eventProc(int)
+{
+}
